f34e888f7ed85e43b874cb51a947caa77d429b27
ksz-ksz <kszkszdev@gmail.com>
2024-06-28 23:27:25 +0200

    fragment

--- packages/routes/src/create-fragment-encoder-factory.ts
+++ packages/routes/src/create-fragment-encoder-factory.ts
@@ -1,9 +1,21 @@
 import { Fragment } from './fragment';
 import { EncoderFactory } from './encoder-factory';
-import { Encoder } from './encoder';
+import { Encoder, EncoderResult } from './encoder';
 
 export function createFragmentEncoderFactory<TParam>(options: {
   param: Encoder<string, TParam>;
 }): EncoderFactory<Fragment, TParam, unknown> {
-  return () => options.param;
+  return () => new FragmentEncoder(options.param);
+}
+
+class FragmentEncoder<TParam> implements Encoder<Fragment, TParam> {
+  constructor(private readonly param: Encoder<string, TParam>) {}
+
+  encode(value: TParam): EncoderResult<Fragment> {
+    return this.param.encode(value);
+  }
+
+  decode(value: Fragment): EncoderResult<TParam> {
+    return this.param.decode(value);
+  }
 }
