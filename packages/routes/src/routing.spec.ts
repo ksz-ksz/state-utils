6b8c28b97f153b6d303d063b01f0bb755fa3c6ab
ksz-ksz <kszkszdev@gmail.com>
2024-07-02 08:11:33 +0200

    routing spec

--- packages/routes/src/routing.spec.ts
+++ packages/routes/src/routing.spec.ts
@@ -8,8 +8,8 @@ import { query } from './create-query-params-encoder-factory';
 import { fragment } from './create-fragment-params-encoder-factory';
 import { params } from './params';
 
-function createTestRouting() {
-  return createRouting({
+function createTestHarness() {
+  const routing = createRouting({
     historian: createBrowserHistorian(),
     pathEncoder: createPathEncoder(),
     queryEncoder: createQueryEncoder(),
@@ -20,67 +20,96 @@ function createTestRouting() {
       fragment: undefined,
     },
   });
+
+  const rootRoute = routing.createRoute({
+    path: path({
+      path: '',
+    }),
+    query: query(),
+    fragment: fragment(),
+  });
+
+  const testParentRoute = routing.createRoute({
+    parent: rootRoute,
+    path: path({
+      path: 'test',
+    }),
+  });
+
+  const testRoute = routing.createRoute({
+    parent: testParentRoute,
+    path: path({
+      path: ':pathParam',
+      params: {
+        pathParam: params.string({
+          pattern: /^valid-path-param$/,
+        }),
+      },
+    }),
+    query: query({
+      params: {
+        queryParam: params.string({
+          pattern: /^valid-query-param$/,
+        }),
+      },
+    }),
+    fragment: fragment({
+      param: params.string({
+        pattern: /^valid-fragment$/,
+      }),
+    }),
+  });
+  return { routing, testRoute };
 }
 
 describe('routing', () => {
   describe('createPlace', () => {
     it('should create place', () => {
-      const routing = createTestRouting();
+      const { routing, testRoute } = createTestHarness();
 
-      const rootRoute = routing.createRoute({
-        path: path({
-          path: '',
-        }),
-        query: query(),
-        fragment: fragment(),
-      });
-
-      const entityRoute = routing.createRoute({
-        parent: rootRoute,
-        path: path({
-          path: 'book',
-        }),
-      });
-
-      const entityDetailsRoute = routing.createRoute({
-        parent: entityRoute,
-        path: path({
-          path: ':bookId',
-          params: {
-            bookId: params.string(),
-          },
-        }),
-        query: query({
-          params: {
-            foo: params.number(),
-          },
-        }),
-        fragment: fragment({
-          param: params.boolean(),
-        }),
-      });
-
-      const place = routing.createPlace(entityDetailsRoute, {
+      const place = routing.createPlace(testRoute, {
         path: {
-          bookId: '1234',
+          pathParam: 'valid-path-param',
         },
         query: {
-          foo: 42,
+          queryParam: 'valid-query-param',
         },
-        fragment: true,
+        fragment: 'valid-fragment',
       });
 
       expect(place).toMatchInlineSnapshot(`
 {
-  "fragment": "true",
+  "fragment": "valid-fragment",
   "path": [
     "",
-    "book",
-    "1234",
+    "test",
+    "valid-path-param",
   ],
   "query": {
-    "foo": "42",
+    "queryParam": "valid-query-param",
   },
+}
+`);
+    });
+
+    it('should create default place if path param is invalid', () => {
+      const { routing, testRoute } = createTestHarness();
+
+      const place = routing.createPlace(testRoute, {
+        path: {
+          pathParam: 'invalid-path-param',
+        },
+        query: {
+          queryParam: 'valid-query-param',
+        },
+        fragment: 'valid-fragment',
+      });
+
+      expect(place).toMatchInlineSnapshot(`
+{
+  "fragment": undefined,
+  "path": [],
+  "query": {},
 }
 `);
     });
