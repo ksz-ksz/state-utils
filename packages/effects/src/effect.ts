52046a303aa05efeba4be9b04bc11bda424ad52a
ksz-ksz <kszkszdev@gmail.com>
2024-07-14 14:44:26 +0200

    Allow returning subscription from effect initializer

--- packages/effects/src/effect.ts
+++ packages/effects/src/effect.ts
@@ -1,5 +1,11 @@
 import { Action, ActionSources } from '@state-utils/actions';
-import { catchError, merge, Observable, throwError } from 'rxjs';
+import {
+  catchError,
+  isObservable,
+  Observable,
+  Subscription,
+  throwError,
+} from 'rxjs';
 import { EffectError } from './effect-error';
 
 export interface Effect {
@@ -7,7 +13,7 @@ export interface Effect {
 }
 
 export interface EffectInitializer {
-  (actionSources: ActionSources): Observable<Action<any>>;
+  (actionSources: ActionSources): Subscription | Observable<Action<any>>;
 }
 
 export type EffectInitializers =
@@ -19,24 +25,32 @@ export function createEffect(
   effectDef: EffectDef
 ): Effect {
   const { name = 'anonymous', effects } = effectDef;
-  const subscription = merge(
-    ...Object.entries(effects).map(([key, initializer]) =>
-      initializer(actionSources).pipe(
-        catchError((e) => throwError(() => new EffectError(name, key, e)))
-      )
-    )
-  ).subscribe({
-    next(action) {
-      actionSources.dispatch(action);
-    },
-    // error(error) {
-    //   queueMicrotask(() => {
-    //     throw new EffectError(name, '*', error);
-    //   });
-    // },
-    // TODO: complete?
-    // TODO: observeOn(queue)?
-  });
+  const subscription = new Subscription();
+  for (const [key, initializer] of Object.entries(effects)) {
+    const result = initializer(actionSources);
+    if (isObservable(result)) {
+      subscription.add(
+        result
+          .pipe(
+            catchError((e) => throwError(() => new EffectError(name, key, e)))
+          )
+          .subscribe({
+            next(action) {
+              actionSources.dispatch(action);
+            },
+            // error(error) {
+            //   queueMicrotask(() => {
+            //     throw new EffectError(name, '*', error);
+            //   });
+            // },
+            // TODO: complete?
+            // TODO: observeOn(queue)?
+          })
+      );
+    } else {
+      subscription.add(result);
+    }
+  }
 
   return {
     dispose() {
